d76fe6b46aed0f82cac49f275cda35fcaa1ba9a0
bintang2629 <bimbimpratama23@gmail.com>
2026-05-14 19:38:03 +0700

    case 1

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -89,6 +89,13 @@ int main()
         cin >> choice;
 
      switch (choice)
+     {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+     }
     }
     
 }

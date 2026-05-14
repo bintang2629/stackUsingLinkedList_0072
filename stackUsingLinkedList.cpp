86663baae7c885ccf5736285d09c5b2230cd86c5
bintang2629 <bimbimpratama23@gmail.com>
2026-05-14 19:31:01 +0700

    current data

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -54,6 +54,15 @@ public:
         {
             cout << "List is empty." << endl;
         }
-        
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
     }
 };

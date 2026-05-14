623f3c58b2da870cc85acc76f13a0f7c05dc064d
bintang2629 <bimbimpratama23@gmail.com>
2026-05-14 19:28:56 +0700

    void pop

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -36,5 +36,17 @@ public:
         return value;
     }
 
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
+
     
 };

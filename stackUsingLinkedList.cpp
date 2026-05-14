728fb14b063cf5001767cc7205eee7a387815cfa
bintang2629 <bimbimpratama23@gmail.com>
2026-05-14 19:38:37 +0700

    case 2

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -95,7 +95,18 @@ int main()
             cin >> value;
             stack.push(value);
             break;
-     }
+      case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        }
     }
+    
     
 }
